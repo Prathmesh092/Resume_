79d1b7478ca8e8cd6db53eb74098d3efda258657
Prathmesh Dollin <prathmeshdollin44@gmail.com>
2025-05-31 08:24:11 +0000

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -47,7 +47,9 @@ const nextConfig: NextConfig = {
       })
     );
 
-
+    module.exports = {
+      allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev'],
+    }
     return config;
   },
 };
